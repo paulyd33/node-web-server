18ad72d7df3dda69c1579b0266d89fbf014bb7f4
Paul Denmead <pauldenmead@hotmail.com>
2019-02-14 02:00:44 +1100

    Disable maintenance mode

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use((req, res, next) => {
   var now = new Date().toString();
